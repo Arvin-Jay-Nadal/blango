a2a895260beddd85b7b1a106c563f6af6462fcf4
Arvin Nadal <arvin.jay_nadal@questronix.com.ph>
2022-09-24 06:54:44 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -103,6 +103,7 @@ class Dev(Configuration):
       "allauth.socialaccount.providers.google",
       "rest_framework",
       "rest_framework.authtoken",
+      "drf_yasg",
   ]
 
   MIDDLEWARE = [
@@ -144,6 +145,13 @@ class Dev(Configuration):
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
 }
+
+  SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+        "Basic": {"type": "basic"},
+    }
+  }
 
   WSGI_APPLICATION = 'blango.wsgi.application'
 
